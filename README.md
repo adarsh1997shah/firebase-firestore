a9ddfc42a599865245cb9706e3d4b93dc5152fd4
adarsh1997shah <adarshshah01@gmail.com>
2020-07-22 20:14:04 +0530

    fixed updating cafes operation

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # firebase-firestore
-> Its a simple project based on firebase firestore for storing data and retreiving data from backened.
+> Its a simple project based on firebase firestore for storing data and retreiving and updating data from backened.
 * The data here is refernced to the name of the cafes and their location.
 
 ## Operations to perform
 * Adding cafes and location
 * Deleting cafes
+* Upadting cafes
+
+### Issues
+* Having problem with re-odering the cafes after performing operations
+
+## Help
+* Any kind of help and consideration is highly motivated.
--- firestore.js
+++ firestore.js
@@ -1,14 +1,11 @@
 const cafeLists = document.getElementById( 'cafe-list' );
-const form = document.getElementById( 'add-cafe-form' );
+const formAddCafe = document.getElementById( 'add-cafe-form' );
+const formEditCafe = document.getElementById( 'edit-cafe-form' );
+const modal = document.getElementById( 'modal' );
+const cancel = document.getElementById( 'edit-cancel' );
+const loader = document.querySelector( '.loader' );
 
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, { dismissible: false });
-});
-
-prompt('sadsa');
 
 // Rendeering Cafes in frontend.
 function renderCafe( doc ) {
@@ -20,12 +17,24 @@ function renderCafe( doc ) {
 				<span>${cafe.name}</span>
 				<span>${cafe.country}</span>
 				<div class="cross waves-effect"><i class="material-icons">cancel</i></div>
-				<div class="edit waves-effect modal-trigger"><i class="material-icons">edit</i></div>
+				<div class="edit waves-effect"><i class="material-icons">edit</i></div>
 			</li>`;
 
 
 	// For deletimg the cafes.
 	deleteCafe();
+
+	// For editing cafes.
+	editCafe();
+}
+
+
+// Re-rendering data after edting.
+function renderEditCafe( doc ) {
+	let child = document.getElementById( doc.id ).children;
+
+	child[0].textContent = doc.data().name;
+	child[1].textContent = doc.data().country;
 }
 
 
@@ -44,9 +53,49 @@ function deleteCafe() {
 }
 
 
+// Editing cafe from frontend.
+function editCafe() {
+	let edits = document.querySelectorAll( '.edit' );
+	
+	edits.forEach( edit => edit.addEventListener( 'click', ( e ) => {
+
+		loader.style.display = 'block';
+
+		let li = e.target.parentElement.parentElement;
+		let id = li.getAttribute( 'id' );
+
+		db.collection( 'Cafes' ).doc( id ).get().then( doc => {
+
+			loader.style.display = 'none';
+
+			let cafe = doc.data();
+
+			formEditCafe.cafeid.value = id;
+			formEditCafe.name.value = cafe.name;
+			formEditCafe.country.value = cafe.country;
+
+
+			modal.classList.remove( 'inactive' );
+			modal.classList.add( 'active' );
+			modal.parentElement.classList.add( 'open' );
+			document.body.classList.add( 'overflow' );
+		} );
+	} ) );
+}
+
+
+
+// Closing cafe details after editing.
+function editClose() {
+	modal.classList.remove( 'active' );
+	modal.classList.add( 'inactive' );
+	modal.parentElement.classList.remove( 'open' );
+	document.body.classList.remove( 'overflow' );
+}
+
 
 // Getting data from firestore (real-time).
-db.collection( 'Cafes' ).orderBy( 'name' ).onSnapshot( ( snapshot ) => {
+let unsubscribe = db.collection( 'Cafes' ).orderBy( 'name' ).onSnapshot( ( snapshot ) => {
 
 	snapshot.docChanges().forEach( ( change ) => {
 
@@ -56,8 +105,13 @@ db.collection( 'Cafes' ).orderBy( 'name' ).onSnapshot( ( snapshot ) => {
 		if( change.type === 'removed' ){
 			console.log('removed');
 		}
+		if( change.type === 'modified' ) {
+			renderEditCafe( change.doc );
+		}
 	} );
-} )
+}, ( error ) => {
+	console.log( 'adasdad', error );
+} );
 
 
 // Getting data from firestore (static).
@@ -85,18 +139,43 @@ db.collection( 'Cafes' ).orderBy( 'name' ).onSnapshot( ( snapshot ) => {
 
 
 // Adding data to firestore.
-form.addEventListener( 'submit', ( e ) => {
+formAddCafe.addEventListener( 'submit', ( e ) => {
 	e.preventDefault();
 
 	// Making sure form is not empty.
-	if( form.name.value != '' && form.country.value != '' ) {
+	if( formAddCafe.name.value != '' && formAddCafe.country.value != '' ) {
 		db.collection( 'Cafes' ).add( {
-			name: form.name.value,
-			country: form.country.value,
+			name: formAddCafe.name.value,
+			country: formAddCafe.country.value,
 		} );
 	} else {
 		alert( 'Either of the two forms is empty!' );
 	}
 
-	form.reset();
+	formAddCafe.reset();
 } );
+
+
+// Editing data to firestore.
+formEditCafe.addEventListener( 'submit', ( e ) => {
+	e.preventDefault();
+
+	// Making sure form is not empty.
+	if( formEditCafe.name.value != '' && formEditCafe.country.value != '' ) {
+		db.collection( 'Cafes' ).doc( formEditCafe.cafeid.value ).update( {
+			name: formEditCafe.name.value,
+			country: formEditCafe.country.value,
+		} );
+	} else {
+		alert( 'Either of the two forms is empty!' );
+	}
+
+	modal.classList.remove( 'active' );
+	modal.classList.add( 'inactive' );
+	modal.parentElement.classList.remove( 'open' );
+	document.body.classList.remove( 'overflow' );
+
+	formEditCafe.reset();
+} );
+
+cancel.addEventListener( 'click', editClose );
